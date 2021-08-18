10f87f974b5eb25586fd59696580620d34e0e375
Ivan Lysukhin <ilysukhin@mail.ru>
2021-08-18 18:03:30 +0300

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>AVTO MOTO</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
